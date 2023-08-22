ef92c37fe206fabe2de7377dfec0f683bf2aefd0
josecardozoif <jose.araujo6@estudante.ifms.edu.br>
2023-08-22 17:12:11 -0400

    Inserindo Toastify

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import handlerAcessUser from "./functions/handlerAcess"
 import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
   const [user, setUser] = useState({
@@ -13,9 +15,13 @@ export default function Login() {
   const handlerLogin = async (e) => {
     e.preventDefault();
     try {
-      await handlerAcessUser(user);
+      const userAuth = await handlerAcessUser(user);
+      if(userAuth.token === undefined){
+        toast.error("Erro no e-mail ou senha!");
+      }
       push('/pages/dashboard');
     } catch {
+      toast.error("Erro no e-mail ou senha!");
       refresh();
     }
   }
@@ -35,6 +41,7 @@ export default function Login() {
         </input>
         <button>Entrar</button>
       </form>
+      <ToastContainer/>
     </div>
   )
 }
